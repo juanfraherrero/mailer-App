de9119075496d2ae70ba6bbbf95994428c83ea3b
Juan Francisco Herrero <juanfraherrero@gmail.com>
2022-03-18 16:44:17 -0300

    modificación de bd

--- app/db.py
+++ app/db.py
@@ -1,4 +1,4 @@
-import mysql.connector
+# import mysql.connector
 import psycopg2
 
 import click
@@ -8,7 +8,7 @@ from .schema import instructions
 
 def get_db():
     if 'db' not in g:
-        g.db = mysql.connector.connect(
+        g.db = psycopg2.connect(
             host=current_app.config['DATABASE_HOST'],
             user=current_app.config['DATABASE_USER'],
             password=current_app.config['DATABASE_PASSWORD'],
