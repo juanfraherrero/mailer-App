deef3511b09fb0540503f8e4e9c3adb32a1f0b20
Juan Francisco Herrero <juanfraherrero@gmail.com>
2022-03-18 16:08:35 -0300

    Modificaciones en db.py y el manejo de la base

--- app/schema.py
+++ app/schema.py
@@ -2,10 +2,10 @@ instructions = [
     'DROP TABLE IF EXISTS email;',
     """
         CREATE TABLE email(
-                id INT PRIMARY KEY AUTO_INCREMENT,
-                email TEXT NOT NULL,
-                subject TEXT NOT NULL,
-                content TEXT NOT NULL
+                id SERIAL PRIMARY KEY,
+                email VARCAHAR(50) NOT NULL,
+                subject VARCAHAR(50) NOT NULL,
+                content VARCAHAR(500) NOT NULL
         );
     """
 
